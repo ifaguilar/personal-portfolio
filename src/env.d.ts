843d5c085fc764acd8cb5e8280dad14e788ed273
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-06-28 13:17:18 -0600

    feat: add env type safety

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_CONTACT_EMAIL: string;
+  readonly PUBLIC_GITHUB_URL: string;
+  readonly PUBLIC_LINKEDIN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
