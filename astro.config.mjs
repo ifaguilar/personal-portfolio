dd114c8cb0366701756de79c8804ac8a89590469
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-06-28 14:41:52 -0600

    fix: import aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-// @ts-check
 import { defineConfig } from "astro/config";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   server: {
@@ -12,6 +16,11 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   },
   integrations: [react(), mdx(), sitemap()],
   adapter: netlify(),
