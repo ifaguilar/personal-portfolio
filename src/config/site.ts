2aa21838264dab194bcecd165a28ab659464f061
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-06-28 17:50:17 -0600

    feat: improve layouts

---
+++ src/config/site.ts
@@ -0,0 +1,6 @@
+export const SITE = {
+  title: "Isaac Aguilar | Software Engineer & Full Stack Developer",
+  description:
+    "Portfolio and projects by Isaac Aguilar." /* TODO: Improve this description */,
+  socialImage: "/og-image.png",
+} as const;
