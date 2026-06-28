0178c04c22146b14fcb009c420921d863f20d211
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-06-28 13:20:22 -0600

    feat: add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ifaguilar-personal-portfolio.netlify.app",
+  integrations: [react(), sitemap(), mdx()],
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
