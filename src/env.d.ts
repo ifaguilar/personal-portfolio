6499052fa831fdc37a85fe10178ca2be1f2d721d
Isaac Aguilar <33670245+ifaguilar@users.noreply.github.com>
2026-07-05 21:41:48 -0600

    feat: improve env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,9 @@ interface ImportMetaEnv {
   readonly PUBLIC_CONTACT_EMAIL: string;
   readonly PUBLIC_GITHUB_URL: string;
   readonly PUBLIC_LINKEDIN_URL: string;
+  readonly RESEND_API_KEY: string;
+  readonly RESEND_FROM_EMAIL: string;
+  readonly RESEND_TO_EMAIL: string;
 }
 
 interface ImportMeta {
